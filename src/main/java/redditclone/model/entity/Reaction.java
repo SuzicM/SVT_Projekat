57aa00f4b4a099f1d298d905c4e65c9c5c8342da
SuzicM <suzicmilan15@gmail.com>
2022-08-18 21:45:19 +0200

    Update Reaction

--- src/main/java/redditclone/model/entity/Reaction.java
+++ src/main/java/redditclone/model/entity/Reaction.java
@@ -1,18 +1,33 @@
 package redditclone.model.entity;
 
+import lombok.*;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="reaction")
 public class Reaction {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long reaction_id;
-	
-	private ReactionType type;
 
+	@Enumerated(EnumType.STRING)
+	@Column(name = "reaction_type")
+	private ReactionType reaction_type;
+
+	@Column
 	private LocalDate time_stamp;
 
+	@Column
 	private long voter_id;
 
+	@Column
 	private long post_id;
 
 }
